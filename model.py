e5e550bcc62237464033d9863fedd02141e0cecf
Mayank Maheshwari <mayankmaheshwari3333@gmail.com>
2019-04-16 22:42:19 +0530

    code that trains our fine tuned resnet model without saving it

--- model.py
+++ model.py
@@ -26,10 +26,10 @@ img_rows, img_cols = 128, 128
 # number of channels
 img_channels = 3
 # number of epochs
-epochs=20
+epochs=25
 
 # number of output classes
-nb_classes = 3
+nb_classes = 5
 
 #  data
 path1_table = '/home/mayank/Desktop/BTP/data/table'    #path of folder of table images    
@@ -41,15 +41,26 @@ path2_plot_processed ='/home/mayank/Desktop/BTP/data/plot_processed'  #path of f
 path1_diagram = '/home/mayank/Desktop/BTP/data/diagram'    #path of folder of diagram images
 path2_diagram_processed ='/home/mayank/Desktop/BTP/data/diagram_processed'  #path of folder to save diagram images
 
+path1_histogram = '/home/mayank/Desktop/BTP/data/histogram'    #path of folder of histogram images
+path2_histogram_processed ='/home/mayank/Desktop/BTP/data/histogram_processed'  #path of folder to save histogram images
+
+path1_graph = '/home/mayank/Desktop/BTP/data/graph'    #path of folder of graph images
+path2_graph_processed ='/home/mayank/Desktop/BTP/data/graph_processed'  #path of folder to save graph images
+
+
 list_table = os.listdir(path1_table)   #Reading all the images from table folder
 list_plot = os.listdir(path1_plot)     #Reading all the images from plot folder
 list_diagram = os.listdir(path1_diagram) #Reading all the images from diagram folder
+list_histogram = os.listdir(path1_histogram) #Reading all the images from histogram folder
+list_graph = os.listdir(path1_graph) #Reading all the images from graph folder
 
 table_samples = size(list_table)        #Getting the total number of Images in Table folder
 plot_samples = size(list_plot)          #Getting the total number of Images in plot folder
 diagram_samples = size(list_diagram)    #Getting the total number of Images in Diagram folder
+histogram_samples = size(list_histogram)#Getting the total number of Images in Histogram folder
+graph_samples = size(list_graph)        #Getting the total number of Images in Graph folder
 
-num_samples = table_samples + plot_samples + diagram_samples #Total number of Images
+num_samples = table_samples + plot_samples + diagram_samples + histogram_samples + graph_samples #Total number of Images
 print(num_samples)
 
 # save the processed table images
@@ -59,7 +70,6 @@ for file in list_table:
     img = img.convert('RGB')          
     img.save(path2_table_processed +'/' +  file, "JPEG")
 
-
 # save the processed plot images
 for file in list_plot:
     im = Image.open(path1_plot + '/' + file)  
@@ -67,13 +77,26 @@ for file in list_plot:
     img = img.convert('RGB')         
     img.save(path2_plot_processed +'/' +  file, "JPEG")
 
-
 # save the processed diagram images
 for file in list_diagram:
     im = Image.open(path1_diagram + '/' + file)  
     img = im.resize((img_rows,img_cols))
     img = img.convert('RGB')          
     img.save(path2_diagram_processed +'/' +  file, "JPEG")
+ 
+#save the processed histogram images
+for file in list_histogram:
+    im = Image.open(path1_histogram + '/' + file)  
+    img = im.resize((img_rows,img_cols))
+    img = img.convert('RGB')          
+    img.save(path2_histogram_processed +'/' +  file, "JPEG")
+
+#save the processed graph images
+for file in list_graph:
+    im = Image.open(path1_graph + '/' + file)  
+    img = im.resize((img_rows,img_cols))
+    img = img.convert('RGB')          
+    img.save(path2_graph_processed +'/' +  file, "JPEG")
 
 
 # img_list = os.listdir(path2)
@@ -120,18 +143,44 @@ diagram_image_number = len(diagram_imglist) # get the number of diagram images
 diagram_imgmatrix = array([array(Image.open(path2_diagram_processed + '/' + diagram_img2)).flatten()
                for diagram_img2 in diagram_imglist],'f')
 
+#Processing for histogram images
+histogram_imglist = os.listdir(path2_histogram_processed)
+print (histogram_imglist)
+histogram_img1 = array(Image.open(path2_histogram_processed + '/'+ histogram_imglist[0])) # open one histogram image to get size
+histogram_m,histogram_n = histogram_img1.shape[0:2] # get the size of the histogram images
+histogram_image_number = len(histogram_imglist) # get the number of histogram images
+
+# create matrix to store all flattened histogram images
+histogram_imgmatrix = array([array(Image.open(path2_histogram_processed + '/' + histogram_img2)).flatten()
+               for histogram_img2 in histogram_imglist],'f')
+
+#Processing for graph images
+graph_imglist = os.listdir(path2_graph_processed)
+print (graph_imglist)
+graph_img1 = array(Image.open(path2_graph_processed + '/'+ graph_imglist[0])) # open one graph image to get size
+graph_m,histogram_n = graph_img1.shape[0:2] # get the size of the graph images
+graph_image_number = len(graph_imglist) # get the number of graph images
+
+# create matrix to store all flattened graph images
+graph_imgmatrix = array([array(Image.open(path2_graph_processed + '/' + graph_img2)).flatten()
+               for graph_img2 in graph_imglist],'f')
 
 
 
 # Combining each matrix
 imgmatrix = numpy.concatenate((table_imgmatrix, plot_imgmatrix), axis=0)
 imgmatrix = numpy.concatenate((imgmatrix, diagram_imgmatrix), axis=0)
+imgmatrix = numpy.concatenate((imgmatrix,histogram_imgmatrix), axis=0)
+imgmatrix = numpy.concatenate((imgmatrix,graph_imgmatrix), axis=0)
 
+class_array = ['Plot','Table','Histogram', 'Graph', 'Diagram']
 
-# label=numpy.ones((num_samples,),dtype = int)
-# label[0:101]=0
-# label[102:201]=1
-# label[202:]=2
+label=numpy.ones((num_samples,),dtype = int)
+label[0:393]=0
+label[393:667]=1
+label[667:804]=2
+label[804:921]=3
+label[921:1029]=4
 
 data,Label = shuffle(imgmatrix,label, random_state=2)
 train_data = [data,Label]
@@ -139,7 +188,6 @@ train_data = [data,Label]
 (X, y) = (train_data[0],train_data[1])
 
 #split X and y into training and testing sets
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2)
 X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
 X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
@@ -164,10 +212,9 @@ X_test -= mean1
 Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
 
-nb_classes = 3
+nb_classes = 5
 
-model = ResNet50(include_top =False, weights='imagenet', input_shape=(128,128,3), pooling='max')
-model.summary()
+model = ResNet50(include_top =False, weights='imagenet', input_shape=(128,128,3), pooling='avg')
 
 x = model.output
 
@@ -176,7 +223,7 @@ x = Dense(512, activation='relu',name='fc-1')(x)
 x = Dropout(0.5)(x)
 x = Dense(256, activation='relu',name='fc-2')(x)
 x = Dropout(0.5)(x)
-# a softmax layer for 4 classes
+# a softmax layer for 5 classes
 out = Dense(nb_classes, activation='softmax',name='output_layer')(x)
 
 # this is the model we will train
@@ -193,7 +240,7 @@ resnet_model2.compile(loss='categorical_crossentropy',optimizer='adam',metrics=[
 
 y_train = y_train.reshape((-1, 1))
 # Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test),batch_size=16, nb_epoch=epochs, verbose=2)
+resnet_model2.fit(X_train, y_train, validation_data=(X_test, y_test),batch_size=32, nb_epoch=epochs, verbose=2)
 # Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print "%s: %.2f%%" % (model.metrics_names[1], scores[1]*100)
+scores = resnet_model2.evaluate(X_test, y_test, verbose=0)
+print "%s: %.2f%%" % (resnet_model2.metrics_names[1], scores[1]*100)
